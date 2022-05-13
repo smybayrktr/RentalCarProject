2c8507135a54c412565bc5451c21d91080078160
SumeyyeBayraktar <smyyebyraktar@gmail.com>
2022-05-13 13:57:18 +0300

    Autofac, AOP and FluentValidation Added

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -19,13 +21,11 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            if (car.Name.Length>2 && car.DailyPrice>0) {
-               _carDal.Add(car);
-                return new SuccessResult(Messages.Added);
-            }
-            return new ErrorResult();
+            _carDal.Add(car);
+            return new SuccessResult(Messages.Added);
         }
 
         public IResult Delete(Car car)
